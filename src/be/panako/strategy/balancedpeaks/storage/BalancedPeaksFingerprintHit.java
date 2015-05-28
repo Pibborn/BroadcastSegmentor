fc210ff3ed8ef81aeccd3249c45c9c66155850cb
Mattia Cerrato <mat-mac@MacBook-Pro-di-Mattia.local>
2015-05-28 15:46:52 +0200

    added packages for new fingerprint algorithms

---
+++ src/be/panako/strategy/balancedpeaks/storage/BalancedPeaksFingerprintHit.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.panako.strategy.balancedpeaks.storage;
+
+/**
+ *
+ * @author Mattia Cerrato <mattia.cerrato@edu.unito.it>
+ */
+public class BalancedPeaksFingerprintHit {
+    
+}
